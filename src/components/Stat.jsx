587cdfcf7ee295023c1d9b512b942d8245c01909
Sam-Tech-Ideas <agyemangsamuel235@gmail.com>
2023-07-09 00:16:57 +0000

    team

--- src/components/Stat.jsx
+++ src/components/Stat.jsx
@@ -1,13 +1,13 @@
 import React from "react";
-import { FaFile, FaFileAlt } from "react-icons/fa";
-
+import { FaFile, FaFileAlt, FaPercent, FaPersonBooth } from "react-icons/fa";
+ import {AiOutlineUsergroupAdd, AiOutlineUsergroupDelete} from "react-icons/ai";
 const Stat = () => {
   return (
     <div
       className=" flex justify-center items-center flex-col"
       style={{
         backgroundImage:
-          "linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(https://firebasestorage.googleapis.com/v0/b/fdsproject-430e7.appspot.com/o/imagesPersoal%2Fback.jpg?alt=media&token=4b7c030b-c96d-42aa-a38c-30bcaddb50c9)",
+          "linear-gradient(rgba(0, 0, 256, 0.2), rgba(0, 0, 0, 0.5)), url(https://firebasestorage.googleapis.com/v0/b/fdsproject-430e7.appspot.com/o/imagesPersoal%2Fback.jpg?alt=media&token=4b7c030b-c96d-42aa-a38c-30bcaddb50c9)",
         backgroundSize: "cover",
         backgroundPosition: "center",
         height: "40vh",
@@ -19,17 +19,19 @@ const Stat = () => {
         <div class="flex-col sm:flex-row sm:justify-around w-full sm:flex items-center">
           <div class="py-8">
             <div>
-              <FaFileAlt size={40} color="white" />
+              <AiOutlineUsergroupAdd size={65} color="white" className="my-8" />
               <h1>
-                <span class="text-white text-4xl font-bold">100</span>
+                <p class="text-white text-4xl font-bold pb-2">100</p>
+                <p className="text-white">Customers</p>
               </h1>
             </div>
           </div>
           <div>
             <div>
-              <FaFileAlt size={40} color="white" />
+              <AiOutlineUsergroupAdd size={65} color="white" className="my-8" />
               <h1>
-                <span class="text-white text-4xl font-bold">100</span>
+                <p class="text-white text-4xl font-bold pb-2">100</p>
+                <p className="text-white">Customers</p>
               </h1>
             </div>
           </div>
@@ -37,17 +39,19 @@ const Stat = () => {
         <div class="flex-col sm:flex-row sm:justify-around w-full sm:flex items-center">
           <div class="py-8">
             <div>
-              <FaFileAlt size={40} color="white" />
+              <AiOutlineUsergroupAdd size={65} color="white" className="my-8 " />
               <h1>
-                <span class="text-white text-4xl font-bold">100</span>
+                <p class="text-white text-4xl font-bold pb-2">100</p>
+                <p className="text-white">Customers</p>
               </h1>
             </div>
           </div>
           <div>
             <div>
-              <FaFileAlt size={40} color="white" />
+              <AiOutlineUsergroupAdd size={65} color="white" className="my-8" />
               <h1>
-                <span class="text-white text-4xl font-bold">100</span>
+                <p class="text-white text-4xl font-bold pb-2">100</p>
+                <p className="text-white">Customers</p>
               </h1>
             </div>
           </div>
--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -1,128 +1,127 @@
-import React from 'react'
+import React from "react";
 
 const Team = () => {
   return (
     <div>
-        <section class="text-gray-600 body-font">
-  <div class="container px-5 py-24 mx-auto">
-    <div class="flex flex-col text-center w-full mb-20">
-      <h1 class="text-2xl font-medium title-font mb-4 text-gray-900">OUR TEAM</h1>
-      <p class="lg:w-2/3 mx-auto leading-relaxed text-base">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table. Franzen you probably haven't heard of them.</p>
+      <section class="text-gray-600 body-font">
+        <div class="container px-5 py-24 mx-auto">
+          <div class="text-center mb-10">
+            <div class="flex mt-6 justify-center py-3">
+              <div class="w-16 h-1 rounded-full bg-blue-500 inline-flex"></div>
+            </div>
+            <h1 class="sm:text-4xl text-3xl font-bold text-gray-900 mb-4">
+              Our Team
+            </h1>
+            <p class="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto text-gray-500s">
+              Meet the team behind the success of our prestigious company.
+            </p>
+          </div>
+          <div class="flex flex-wrap -m-4">
+            <div class="p-4 lg:w-1/6 md:w-1/2">
+              <div class="h-full flex flex-col items-center text-center">
+                <img
+                  alt="team"
+                  class="flex-shrink-0 rounded-lg w-full    object-cover object-center mb-4"
+                  src="https://images.unsplash.com/photo-1688378911966-ff12184b2680?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyOXx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+                />
+                <div class="w-full">
+                  <h2 class="title-font font-medium text-lg text-gray-900">
+                    EUNICE BAAFI
+                  </h2>
+                  <h3 class="text-gray-500 mb-3">
+                    CHIEF EXECUTIVE OFFICER (PLASTICS)
+                  </h3>
+                </div>
+              </div>
+            </div>
+            <div class="p-4 lg:w-1/6 md:w-1/2">
+              <div class="h-full flex flex-col items-center text-center">
+                <img
+                  alt="team"
+                  class="flex-shrink-0 rounded-lg w-full  object-cover object-center mb-4"
+                  src="https://images.unsplash.com/photo-1688378911966-ff12184b2680?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyOXx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+                />
+                <div class="w-full">
+                  <h2 class="title-font font-medium text-lg text-gray-900">
+                    STEPHEN BAAFI
+                  </h2>
+                  <h3 class="text-gray-500 mb-3">
+                    CHIEF EXECUTIVE DIRECTOR (B. Hardware)
+                  </h3>
+                </div>
+              </div>
+            </div>
+            <div class="p-4 lg:w-1/6 md:w-1/2">
+              <div class="h-full flex flex-col items-center text-center">
+                <img
+                  alt="team"
+                  class="flex-shrink-0 rounded-lg w-full  object-cover object-center mb-4"
+                  src="https://images.unsplash.com/photo-1688378911966-ff12184b2680?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyOXx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+                />
+                <div class="w-full">
+                  <h2 class="title-font font-medium text-lg text-gray-900">
+                    LEWIS BAAFI
+                  </h2>
+                  <h3 class="text-gray-500 mb-3">PRESIDENT</h3>
+                  <h3 class="text-gray-500 mb-3">DIGITAL CONTENT MANAGER</h3>
+                  <h3 class="text-gray-500 mb-3">MARKETING DIRECTOR</h3>
+                </div>
+              </div>
+            </div>
+            <div class="p-4 lg:w-1/6 md:w-1/2">
+              <div class="h-full flex flex-col items-center text-center">
+                <img
+                  alt="team"
+                  class="flex-shrink-0 rounded-lg w-full   object-cover object-center mb-4"
+                  src="https://images.unsplash.com/photo-1688378911966-ff12184b2680?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyOXx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+                />
+                <div class="w-full">
+                  <h2 class="title-font font-medium text-lg text-gray-900">
+                    JESSICA BAAFI
+                  </h2>
+                  <h3 class="text-gray-500 mb-3">VICE PRESIDENT</h3>
+                </div>
+              </div>
+            </div>
+            <div class="p-4 lg:w-1/6 md:w-1/2">
+              <div class="h-full flex flex-col items-center text-center">
+                <img
+                  alt="team"
+                  class="flex-shrink-0 rounded-lg w-full  object-cover object-center mb-4"
+                  src="https://images.unsplash.com/photo-1688378911966-ff12184b2680?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyOXx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+                />
+                <div class="w-full">
+                  <h2 class="title-font font-medium text-lg text-gray-900">
+                    MAGRET
+                  </h2>
+                  <h3 class="text-gray-500 mb-3">
+                    MANAGING DIRECTOR RETAIL &amp; WHOLESALE TRADE
+                  </h3>
+                </div>
+              </div>
+            </div>
+            <div class="p-4 lg:w-1/6 md:w-1/2">
+              <div class="h-full flex flex-col items-center text-center">
+                <img
+                  alt="team"
+                  class="flex-shrink-0 rounded-lg w-full  object-cover object-center mb-4"
+                  src="https://images.unsplash.com/photo-1688378911966-ff12184b2680?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyOXx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+                />
+                <div class="w-full">
+                  <h2 class="title-font font-medium text-lg text-gray-900">
+                    FOSTER
+                  </h2>
+                  <h3 class="text-gray-500 mb-3">
+                    MANAGING DIRECTOR RETAIL &amp; WHOLESALE TRADE
+                  </h3>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
-    <div class="flex flex-wrap -m-4">
-      <div class="p-4 lg:w-1/4 md:w-1/2">
-        <div class="h-full flex flex-col items-center text-center">
-          <img alt="team" class="flex-shrink-0 rounded-lg w-full h-56 object-cover object-center mb-4" src="https://dummyimage.com/200x200"/>
-          <div class="w-full">
-            <h2 class="title-font font-medium text-lg text-gray-900">Alper Kamu</h2>
-            <h3 class="text-gray-500 mb-3">UI Developer</h3>
-            <p class="mb-4">DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.</p>
-            <span class="inline-flex">
-              <a class="text-gray-500">
-                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
-                  <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
-                </svg>
-              </a>
-              <a class="ml-2 text-gray-500">
-                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
-                  <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
-                </svg>
-              </a>
-              <a class="ml-2 text-gray-500">
-                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
-                  <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
-                </svg>
-              </a>
-            </span>
-          </div>
-        </div>
-      </div>
-      <div class="p-4 lg:w-1/4 md:w-1/2">
-        <div class="h-full flex flex-col items-center text-center">
-          <img alt="team" class="flex-shrink-0 rounded-lg w-full h-56 object-cover object-center mb-4" src="https://dummyimage.com/201x201"/>
-          <div class="w-full">
-            <h2 class="title-font font-medium text-lg text-gray-900">Holden Caulfield</h2>
-            <h3 class="text-gray-500 mb-3">UI Developer</h3>
-            <p class="mb-4">DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.</p>
-            <span class="inline-flex">
-              <a class="text-gray-500">
-                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
-                  <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
-                </svg>
-              </a>
-              <a class="ml-2 text-gray-500">
-                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
-                  <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
-                </svg>
-              </a>
-              <a class="ml-2 text-gray-500">
-                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
-                  <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
-                </svg>
-              </a>
-            </span>
-          </div>
-        </div>
-      </div>
-      <div class="p-4 lg:w-1/4 md:w-1/2">
-        <div class="h-full flex flex-col items-center text-center">
-          <img alt="team" class="flex-shrink-0 rounded-lg w-full h-56 object-cover object-center mb-4" src="https://dummyimage.com/202x202"/>
-          <div class="w-full">
-            <h2 class="title-font font-medium text-lg text-gray-900">Atticus Finch</h2>
-            <h3 class="text-gray-500 mb-3">UI Developer</h3>
-            <p class="mb-4">DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.</p>
-            <span class="inline-flex">
-              <a class="text-gray-500">
-                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
-                  <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
-                </svg>
-              </a>
-              <a class="ml-2 text-gray-500">
-                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
-                  <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
-                </svg>
-              </a>
-              <a class="ml-2 text-gray-500">
-                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
-                  <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
-                </svg>
-              </a>
-            </span>
-          </div>
-        </div>
-      </div>
-      <div class="p-4 lg:w-1/4 md:w-1/2">
-        <div class="h-full flex flex-col items-center text-center">
-          <img alt="team" class="flex-shrink-0 rounded-lg w-full h-56 object-cover object-center mb-4" src="https://dummyimage.com/203x203"/>
-          <div class="w-full">
-            <h2 class="title-font font-medium text-lg text-gray-900">Henry Letham</h2>
-            <h3 class="text-gray-500 mb-3">UI Developer</h3>
-            <p class="mb-4">DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.</p>
-            <span class="inline-flex">
-              <a class="text-gray-500">
-                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
-                  <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
-                </svg>
-              </a>
-              <a class="ml-2 text-gray-500">
-                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
-                  <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
-                </svg>
-              </a>
-              <a class="ml-2 text-gray-500">
-                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
-                  <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
-                </svg>
-              </a>
-            </span>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-</section>
-    </div>
-  )
-}
+  );
+};
 
-export default Team
+export default Team;
