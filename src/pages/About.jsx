781b699aa1fedcdc53dce1038b2a68ecb5dfeb87
Sam-Tech-Ideas <agyemangsamuel235@gmail.com>
2023-07-28 09:20:22 +0000

    update

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -19,7 +19,7 @@ const About = () => {
       </div>
       <div class="container  mx-auto md:px-6">
         <section class="">
-          <div class="block rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700">
+          <div class="block rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)]">
             <div class="flex flex-wrap items-center">
               <div class="hidden shrink-0 grow-0 basis-auto lg:flex lg:w-6/12 xl:w-4/12">
                 <img
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,31 @@
-const withMT = require("@material-tailwind/react/utils/withMT");
+// const withMT = require("@material-tailwind/react/utils/withMT");
 
-module.exports = withMT({
-  content: [
+// module.exports = withMT({
+//   content: [
+//     "./index.html",
+//     "./src/**/*.{vue,js,ts,jsx,tsx}",
+//     "./node_modules/@material-tailwind/react/dist/**/*.{vue,js,ts,jsx,tsx}",
+//   ],
+
+//   theme: {
+//     extend: {
+//       colors: {
+//         primary: "#022d36",
+//       },
+//     },
+//   },
+//   plugins: [],
+// });
+
+{/**tailwind config */}
+
+module.exports = {
+  purge: [
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
     "./node_modules/@material-tailwind/react/dist/**/*.{vue,js,ts,jsx,tsx}",
   ],
-
+  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       colors: {
@@ -14,5 +33,8 @@ module.exports = withMT({
       },
     },
   },
-  plugins: [],
-});
+  variants: {
+    extend: {},
+  },
+};
+
